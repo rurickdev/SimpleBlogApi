7814245210a132c6161e66a18f52a8f7bb9d3df2
Rurick M. Poisot <skintigthvader@gmail.com>
2019-09-12 21:14:14 -0500

    use router

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 
 const express = require('express')
 
+const router = require('./routers/posts')
+
 const app = express()
 
 app.use(express.json())
+
+app.use('/posts', router)
 
 app.get('/', (request, response) => {
   response.json({
