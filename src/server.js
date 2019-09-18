34d14f1a692ecfc58e7ec831f13b36c6d6e57b1a
Rurick M. Poisot <skintigthvader@gmail.com>
2019-09-17 20:08:00 -0500

    Implemented JWS and Cors

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 
 const express = require('express')
+const cors = require('cors')
 
 const router = require('./routers/posts')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 
 app.use('/posts', router)
