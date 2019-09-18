37de7fbe36ca71af296746ee69bebd5e222b57fe
Rurick M. Poisot <skintigthvader@gmail.com>
2019-09-17 20:37:35 -0500

    use try catchs

--- src/routers/users.js
+++ src/routers/users.js
@@ -5,54 +5,105 @@ const user = require('../usecases/users')
 
 const router = express.Router()
 
+router.get('/', async (request, response) => {
+  try {
+    const allUsers = await user.getAll()
+    response.json({
+      success: true,
+      message: 'All users',
+      data: {
+        users: allUsers
+      }
+    })
+  } catch (error) {
+    response.json({
+      success: false,
+      message: 'Something failed',
+      data: {
+        error
+      }
+    })
+  }
+})
+
 router.get('/:id', async (request, response) => {
-  const { id } = request.params
+  try {
+    const { id } = request.params
 
-  const gotUser = await user.getById(id)
+    const gotUser = await user.getById(id)
 
-  response.json({
-    success: true,
-    message: 'Author Obtained',
-    data: {
-      post: gotUser
-    }
-  })
+    response.json({
+      success: true,
+      message: 'Author Obtained',
+      data: {
+        post: gotUser
+      }
+    })
+  } catch (error) {
+    response.json({
+      success: false,
+      message: 'Something failed',
+      data: {
+        error
+      }
+    })
+  }
 })
 
 router.post('/', async (request, response) => {
-  const {
-    name,
-    email,
-    password
-  } = request.body
+  try {
+    const {
+      name,
+      email,
+      password
+    } = request.body
 
-  const newUser = await user.create({
-    name,
-    email,
-    password
-  })
+    const newUser = await user.create({
+      name,
+      email,
+      password
+    })
 
-  response.json({
-    success: true,
-    message: 'User Created',
-    data: {
-      post: newUser
-    }
-  })
+    response.json({
+      success: true,
+      message: 'User Created',
+      data: {
+        post: newUser
+      }
+    })
+  } catch (error) {
+    response.json({
+      success: false,
+      message: 'Something failed',
+      data: {
+        error
+      }
+    })
+  }
 })
 
 router.delete('/:id', async (request, response) => {
-  const { id } = request.params
+  try {
+    const { id } = request.params
 
-  const deletedUser = await user.deleteById(id)
+    const deletedUser = await user.deleteById(id)
 
-  response.json({
-    success: true,
-    message: 'User deleted',
-    data: {
-      post: deletedUser
-    }
-  })
+    response.json({
+      success: true,
+      message: 'User deleted',
+      data: {
+        post: deletedUser
+      }
+    })
+  } catch (error) {
+    response.json({
+      success: false,
+      message: 'Something failed',
+      data: {
+        error
+      }
+    })
+  }
 })
 
 module.exports = router
